f7c57be69b216ce9a19901eaf03df30e2ea46b3d
kanchuharish5 <kanchuharish@gmail.com>
2026-06-15 13:08:37 -0400

    Update streamlit_app.py
    Added API calls to get fruit details

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests  
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
@@ -54,7 +55,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order , icon="✅")
 
-
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
     
 
     
